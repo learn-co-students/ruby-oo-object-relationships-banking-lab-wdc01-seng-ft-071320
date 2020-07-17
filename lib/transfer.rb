153979b9e1c86b7fb8e2e91ab3f85de7ac1b31d4
Claudia Borghini <claudiaborghini@Claudias-MacBook-Air.local>
2020-07-17 11:25:40 +0200

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,22 +16,27 @@ class Transfer
   end
 
   def execute_transaction  
-    if self.valid? && self.sender.balance > self.amount
+
+    if self.status == "pending" && self.valid? && self.sender.balance > self.amount
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
-      self.amount = 0
+
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
+
     end
+  
   end
 
   def reverse_transfer
-    if self.execute_transaction
-      self.receiver.balance -= self.amount
+    if self.status == "complete"
+      self.receiver.balance = self.receiver.balance - self.amount
+      #binding.pry
       self.sender.balance += self.amount
       self.status = "reversed"
+      #binding.pry
     end
   end
 
