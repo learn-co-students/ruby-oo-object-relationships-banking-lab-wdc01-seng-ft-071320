df3d48213b8148b49948de77ea6dcd6b44987972
Claudia Borghini <claudiaborghini@Claudias-MacBook-Air.local>
2020-07-17 11:02:54 +0200

    Almost Done

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+#@@all_bank_accounts = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    #@@all_bank_accounts << self
+  end
+
+# def all
+#     @@all_bank_accounts
+# end
+
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
 
 end
+
+
+#binding.pry
