c2e3bbe9923c6679d7d596b5b9ba0e5b250d359a
JinaStrickland <Jina30@gmail.com>
2020-07-16 21:10:30 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require_relative "./bank_account.rb"
+require "pry"
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+  end
+
+  def valid?
+      if sender.balance > 0 && receiver.balance > 0
+        return true
+      # else
+      #   return false
+      end
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+
+  end
+
+
 end
